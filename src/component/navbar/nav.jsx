4d218f55a86684b4cc0051cc6c97d114e653d28d
almiraazs <aalmirafiraa@gmail.com>
2025-05-19 23:23:46 +0700

    navbar

---
+++ src/component/navbar/nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const nav = () => {
+  return (
+    <div className="py-4">
+        <div className="container">
+            {/* logo */}
+            <div>
+                <p className="text-lg font-semibold">
+                    Elite <span className="text-orange-600">Trade Hub</span>
+                </p>
+            </div>
+            {/* menu */}
+        </div>
+    </div>
+  )
+}
+
+export default nav
